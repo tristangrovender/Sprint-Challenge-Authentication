e4e3ce8c2cbbe30d16587af40864943d2b513ed9
Tristan Grovender <therealtristangrovender@gmail.com>
2019-05-10 11:24:22 -0500

    added users model, register, and login

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -9,12 +10,44 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
-}
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+};
+
 
 function login(req, res) {
-  // implement user login
-}
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenService.generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!, have a token...`,
+          token,
+          roles: token.roles,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+};
+
+
+  
 
 function getJokes(req, res) {
   const requestOptions = {
