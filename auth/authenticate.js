a9991675c18270bad50a65a4844495ca405c4201
Tristan Grovender <therealtristangrovender@gmail.com>
2019-05-10 11:46:43 -0500

    MVP complete, secrets moved to own file, authenticate refactored, routes re-worked, token generated, tested with insomnia, general debugging

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,8 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-const jwtKey =
-  process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+const secrets = require("../config/secrets.js");
+
 
 // quickly see what this file exports
 module.exports = {
@@ -11,10 +10,10 @@ module.exports = {
 
 // implementation details
 function authenticate(req, res, next) {
-  const token = req.get('Authorization');
+  const token = req.headers.authorization;
 
   if (token) {
-    jwt.verify(token, jwtKey, (err, decoded) => {
+    jwt.verify(token, secrets.jwtKey, (err, decoded) => {
       if (err) return res.status(401).json(err);
 
       req.decoded = decoded;
--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,8 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model.js');
+const secrets = require("../config/secrets.js");
+const jwt = require("jsonwebtoken");
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -27,26 +30,37 @@ function register(req, res) {
 function login(req, res) {
   let { username, password } = req.body;
 
-  Users.findBy({ username })
+   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        const token = tokenService.generateToken(user);
-        res.status(200).json({
-          message: `Welcome ${user.username}!, have a token...`,
-          token,
-          roles: token.roles,
+        const token = generateToken(user);
+
+         res.status(200).json({
+          message: `Welcome ${
+            user.username
+          }!, we have been waiting here\'s your token, proceed with caution`,
+          token
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Access Denied!" });
       }
     })
-    .catch(error => {
-      res.status(500).json(error);
+    .catch(err => {
+      res.status(500).json(err);
     });
-};
+}
 
-
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  return jwt.sign(payload, secrets.jwtKey, options);
+}
   
 
 function getJokes(req, res) {
